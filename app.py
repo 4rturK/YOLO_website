c3c6f19efdd58406942b045189b05d78457b2813
SzymonCieslar24 <cieslarszymon24@gmail.com>
2025-12-01 17:51:50 +0100

    Added feature that use basic pretrained YOLO model for object detection.

--- app.py
+++ app.py
@@ -1,38 +1,68 @@
 from flask import Flask, render_template, request, send_from_directory
 import os
 from PIL import Image
+from ultralytics import YOLO
+import uuid
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
-
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
+model = YOLO('yolov8n.pt') 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     uploaded_image = None
+    processed_image = None
+    detections = []
 
     if request.method == 'POST':
         file = request.files.get('image')
 
-        if file:
-            img_path = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded.jpg')
+        if file and file.filename != '':
+            unique_filename = f"{uuid.uuid4().hex}_{file.filename}"
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
             file.save(img_path)
 
-            uploaded_image = img_path.replace("static/", "")
+            uploaded_image = unique_filename
 
             # --------------------------
-            # YOLO
+            # Analiza YOLO
             # --------------------------
+            results = model(img_path, conf=0.25) 
 
-    return render_template("index.html", uploaded_image=uploaded_image)
+            result = results[0]
 
+            im_array = result.plot() 
+            im = Image.fromarray(im_array[..., ::-1])
+            
+            processed_filename = f"pred_{unique_filename}"
+            processed_path = os.path.join(app.config['UPLOAD_FOLDER'], processed_filename)
+            im.save(processed_path)
+            
+            processed_image = processed_filename
+
+            for box in result.boxes:
+                class_id = int(box.cls[0])
+                class_name = model.names[class_id]
+                confidence = float(box.conf[0])
+                
+                detections.append({
+                    "name": class_name,
+                    "confidence": round(confidence * 100, 2)
+                })
+
+    return render_template(
+        "index.html", 
+        uploaded_image=uploaded_image, 
+        processed_image=processed_image,
+        detections=detections
+    )
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
